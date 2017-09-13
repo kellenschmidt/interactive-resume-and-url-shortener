97bc9fdc6636ed1fbd3db29ead4bd5c23a8a14ba
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-09-13 13:46:13 -0500

    Updated mdb and added ngFor for courses carousel

--- src/app/interactive-resume/courses/courses.component.ts
+++ src/app/interactive-resume/courses/courses.component.ts
@@ -7,6 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
+  courseGroups = [
+    [
+      {
+      id: 0,
+      number: "CSE 5345",
+      name: "Advanced App Programming"
+      },
+      {
+        id: 1,
+        number: "CSE 5323",
+        name: "Mobile Apps for Sensing and Learning"
+      },
+      {
+        id: 2,
+        number: "CSE 4345",
+        name: "Software Engineering Principles"
+      },
+    ],
+    [
+      {
+        id: 3,
+        number: "CSE 3330",
+        name: "Database Concepts"
+      },
+      {
+        id: 4,
+        number: "CSE 2341",
+        name: "Data Structures"
+      },
+      {
+        id: 5,
+        number: "CSE 3345",
+        name: "Graphical User Interface Design"
+      },
+    ],
+  ]
+
   constructor() { }
 
   ngOnInit() {
