f932e6160fa259118b206096908bde35204171a4
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-12-22 14:41:58 -0700

    Add compatibility with old php url shortener links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules }  from '@angular/router';
+import { PhpRedirectComponent } from 'app/php-redirect.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'interactive-resume', pathMatch: 'full' },
   { path: 'about', redirectTo: 'interactive-resume', pathMatch: 'full' },
   { path: 'home', redirectTo: 'interactive-resume', pathMatch: 'full' },
   { path: 'interactive-resume', loadChildren: 'app/interactive-resume/interactive-resume.module#InteractiveResumeModule' },
+  { path: 'php', component: PhpRedirectComponent },
+  { path: 'php/:code', component: PhpRedirectComponent },
   { path: 'url', redirectTo: 'url-shortener', pathMatch: 'full' },
   { path: 'urls', redirectTo: 'url-shortener', pathMatch: 'full' },
   { path: 'url-shortener', loadChildren: 'app/url-shortener/url-shortener.module#UrlShortenerModule' },
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PhpRedirectComponent,
+  ],
   imports: [
     RouterModule.forRoot(
       appRoutes,
