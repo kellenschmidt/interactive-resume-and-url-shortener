f13e9a3d070aff02e8c202cc40eeac9a79a70948
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-05-21 08:43:56 -0600

    Changed comments to match new environment name

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=docker` then `environment.docker.ts` will be used instead.
+// `ng build --env=dev` then `environment.dev.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
